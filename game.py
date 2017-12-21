3b85531b979d3cbe662385287d2cf149a196d9c0
AsbjornOlling <asbjornolling@gmail.com>
2017-12-21 16:46:52 +0300

    cleaned up game.py

--- game.py
+++ game.py
@@ -1,56 +1,47 @@
-# GAME
-# conatainer file for game states, clock etc.
-
+# container file for game states, rooms, main clock etc.
 import pygame
 import view
 import events
 
-# time stuff
+# time vars
 clock = pygame.time.Clock()
 dt = 0
 
-# sprite groups
+# global sprite groups
 allsprites = pygame.sprite.RenderUpdates()
 singleplayer = pygame.sprite.GroupSingle()
 hardcollide = pygame.sprite.Group()
 
-
-# contains walls, enemies, pickups, etc
+# contains a sprite group w/ walls
 class Room:
     wallthickness = 20
 
     def __init__(self):
-        # list of walls
+        # sprite groups
         self.walls = pygame.sprite.Group()
 
-        # make walls
-        self.wall_w = Wall((0, 0), 
-                            (self.wallthickness, view.vw.height))
+        # walls along screen edges
+        self.wall_w = Wall((0, 0), (self.wallthickness, view.vw.height))
+        self.wall_e = Wall((view.vw.width - self.wallthickness, 0), (self.wallthickness, view.vw.height))
+        self.wall_n = Wall((0, 0), (view.vw.width, self.wallthickness))
+        self.wall_s = Wall((0, view.vw.height - self.wallthickness), (view.vw.width, self.wallthickness))
+
         self.walls.add(self.wall_w)
-
-        self.wall_e = Wall((view.vw.width - self.wallthickness, 0),
-                            (self.wallthickness, view.vw.height))
         self.walls.add(self.wall_e)
-
-        self.wall_n = Wall((0, 0),
-                            (view.vw.width, self.wallthickness))
         self.walls.add(self.wall_n)
-
-        self.wall_s = Wall((0, view.vw.height - self.wallthickness),
-                            (view.vw.width, self.wallthickness))
         self.walls.add(self.wall_s)
 
+        # a destructible block
         self.wall_c = WallDestructible((150, 150), (50, 50))
         self.walls.add(self.wall_c)
 
 
-# just a wall sprite, for the player to bounce off
-# nothing but position and size
+# dumb block, for the player to bounce off
 class Wall(pygame.sprite.Sprite):
     def __init__(self, coordtuple, sizetuple):
         pygame.sprite.Sprite.__init__(self)
 
-        # groups
+        # sprite groups
         allsprites.add(self)
         hardcollide.add(self)
         
@@ -61,16 +52,18 @@ class Wall(pygame.sprite.Sprite):
         self.rect = pygame.Rect(coordtuple, sizetuple)
 
 
+# a dumb block, that takes damage from thrusters
 class WallDestructible(Wall):
     def __init__(self, coordtuple, sizetuple):
         super(WallDestructible, self).__init__(coordtuple, sizetuple)
         self.health = 100
 
-
+    # run on every tick
     def update(self):
-        # look for collision with player thrusters
+        # detect collision with player thrusters
         collisions = pygame.sprite.spritecollide(self, singleplayer.sprite.thrusters, 0)
         for thruster in collisions:
+            # subtract health
             self.health -= thruster.length * dt / 1000
 
         # kill if no health
@@ -79,6 +72,6 @@ class WallDestructible(Wall):
             events.evm.notify(events.ObjDeath(self.rect))
             self.kill()
 
-
+# make a room (temp)
 # for room testing
 currentroom = Room()
