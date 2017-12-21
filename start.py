0f805b94a1dd6760bb0e916b7e7cec16667c90e0
AsbjornOlling <asbjornolling@gmail.com>
2017-12-21 17:30:33 +0300

    clean up start.py

--- start.py
+++ start.py
@@ -1,48 +1,35 @@
 # Thruster game
 # WIP
 
-import pygame
-from pygame.locals import *
-
-import game
-import view
-import events
-import controls
-import player
-
 class App:
     def __init__(self):
-        # pygame stuff
         pygame.init()
-        pygame.key.set_repeat(50, 30)
 
-        # display settings
+        # display caption
         pygame.display.set_caption("THRUSTER")
 
         # make player obj
         self.p = player.Player()
 
+    # receive events from evm
+    def notify(self, event):
+        if isinstance(event, events.Quit):
+            self.cleanup()
 
-    def on_event(self):
-        # main event handler
-        for event in pygame.event.get():
-            # exit game
-            if event.type == pygame.QUIT:
-                self.on_cleanup()
-
-
-    def on_cleanup(self):
+    def cleanup(self):
         pygame.quit()
         quit()
 
 
-    # receive events from evm
-    def notify(self, event):
-        if isinstance(event, events.Quit):
-            self.on_cleanup()
-
-
 if __name__ == "__main__":
+    import pygame
+    from pygame.locals import *
+    import game
+    import view
+    import events
+    import controls
+    import player
+
     app = App()
     kb = controls.KeyboardController()
     
@@ -53,11 +40,10 @@ if __name__ == "__main__":
     while True:
         # handle keyboard events
         kb.update()
-        app.on_event() # legacy
         # update all sprites
         game.allsprites.update()
         # draw everything
-        view.vw.render()
+        view.vw.update()
 
         # tick
         game.dt = game.clock.tick(view.vw.fps)
