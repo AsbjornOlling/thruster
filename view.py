32f1a2dd8716d5f5b9f6679688add09466c19b9a
AsbjornOlling <asbjornolling@gmail.com>
2017-12-21 16:30:00 +0300

    cleaned up view.py

--- view.py
+++ view.py
@@ -4,7 +4,7 @@ import game
 import events
 
 class Viewer():
-    # some constants
+    # resolution and framerate
     width = 500
     height = 500
     fps = 60
@@ -14,33 +14,22 @@ class Viewer():
     color_flame = (128, 0, 0)       # just red
     color_wall = (128, 128, 128)    # gray
 
-
     def __init__(self):
-        # pygame display surface
+        # display surface
         self.screen = pygame.display.set_mode((self.width, self.height))
 
-        # add listener
+        # event listener
         events.evm.add_listener(self)
 
         # lists of areas to update
         self.update_rects = []
         self.update_rects_next = []
 
-
-    def notify(self, event):
-        if isinstance(event, events.WallDeath):
-            # draw over wall
-            self.update_rects.append(event.rect)
-
-
-    def render(self):
-        # clear screen
+    # run on every tick
+    def update(self):
+        # lay background first
         self.screen.fill(self.color_bg)
-
-        # draw all sprites and get rects
-        for rect in game.allsprites.draw(self.screen):
-            self.update_rects.append(rect)
-
+        self.draw_sprites()
         self.draw_walls()
         self.draw_thrusters()
 
@@ -53,21 +42,30 @@ class Viewer():
             self.update_rects.append(rect)
         self.update_rects_next = []
 
+    # handle events
+    def notify(self, event):
+        # make sure to update dead wall
+        if isinstance(event, events.WallDeath):
+            self.update_rects.append(event.rect)
     
-    # draw player's thrusters, add rects to lists
-    def draw_thrusters(self):
-        for thruster in game.singleplayer.sprite.thrusters:
-            pygame.draw.ellipse(self.screen, self.color_flame, thruster.rect)
-            self.update_rects.append(thruster.rect)
-            self.update_rects_next.append(thruster.rect)
+    # draw sprites and get update rects
+    def draw_sprites(self):
+        for rect in game.allsprites.draw(self.screen):
+            self.update_rects.append(rect)
 
-    
-    # draw current room's walls, add rects to lists
+    # draw the walls of currentroom and get update rects
     def draw_walls(self):
         room = game.currentroom
         for wall in room.walls:
             pygame.draw.rect(self.screen, self.color_wall, wall.rect)
             self.update_rects.append(wall.rect)
+
+    # draw player's thrusters and get update rects
+    def draw_thrusters(self):
+        for thruster in game.singleplayer.sprite.thrusters:
+            pygame.draw.ellipse(self.screen, self.color_flame, thruster.rect)
+            self.update_rects.append(thruster.rect)
+            self.update_rects_next.append(thruster.rect)
 
 
 # the actual viewer object
