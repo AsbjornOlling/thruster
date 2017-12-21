d806af84284751b7154feb53f8613716d025791e
AsbjornOlling <asbjornolling@gmail.com>
2017-12-21 16:39:36 +0300

    cleaned up controls.py

--- controls.py
+++ controls.py
@@ -1,28 +1,27 @@
-# handle keyboard (maybe mouse / gamepad in the future)
+# handle input
 import pygame
 from pygame.locals import *
 import events
 
 class KeyboardController:
-    def __init__(self):
-        pass
-
+    # run on every tick
     def update(self):
-        # single press key events
+
+        # single-press events
         for event in pygame.event.get():
             if event.type == KEYDOWN:
+                # Q: exit game
                 if event.key == K_q:
                     events.evm.notify(events.Quit())
-                    # send quit event
 
         # get held keys
         keystate = pygame.key.get_pressed()
-
+        
+        # arrow key movement
         if keystate[K_RIGHT]:
             events.evm.notify(events.PlayerThrust("E"))
         elif keystate[K_LEFT]:
             events.evm.notify(events.PlayerThrust("W"))
-
         if keystate[K_UP]:
             events.evm.notify(events.PlayerThrust("N"))
         elif keystate[K_DOWN]:
