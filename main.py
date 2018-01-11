f8f041044cfb8060082d7c53e1b690bc33cd5c35
AsbjornOlling <asbjornolling@gmail.com>
2018-01-11 22:41:11 +0100

    cleaned up main.py

--- main.py
+++ main.py
@@ -11,52 +11,39 @@ class App:
     # design resolution
     WIDTH, HEIGHT = RES = (1280, 720)
 
-    def __init__(self, eventmanager):
+    def __init__(self):
         pg.init()
-        # window title
         pg.display.set_caption("THRUSTER")
 
-        # event manager
-        self.evm = eventmanager
+        self.evm = events.EventManager()                    # communication between objects
         self.evm.add_listener(self)
+        self.gm = game.Game(self.RES, self.evm)             # game model
+        self.vw = view.Viewer(self.RES, self.gm, self.evm)  # visuals
+        self.kb = controls.KeyboardController(self.evm)     # handle keyboard events
 
-    # receive events from evm
+
+    def run(self):
+        # create room, player
+        self.gm.start()
+        while True:
+            self.kb.update()  # get keyboard events
+            self.gm.update()  # update game model
+            self.vw.update()  # draw for the player
+            self.gm.tick()    # wait 1/60th second
+
+
+    # handle events received
     def notify(self, event):
         if isinstance(event, events.Quit):
             self.cleanup()
 
+
+    # run on exit
     def cleanup(self):
         pg.quit()
         quit()
 
 
 if __name__ == "__main__":
-    # mediator
-    evm = events.EventManager()
-
-    # main app object
-    app = App(evm)
-
-    # model
-    gm = game.Game(app.RES, evm)
-
-    # viewer
-    vw = view.Viewer(app.RES, gm, evm)
-
-    # create room, player
-    gm.start()
-
-    # controller
-    kb = controls.KeyboardController(evm)
-    
-
-    # main loop
-    while True:
-        # handle keyboard events
-        kb.update()
-        # update all sprites
-        gm.update()
-        # draw everything
-        vw.update()
-
-        gm.tick()
+    app = App()
+    app.run()
